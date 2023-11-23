eb9afe50150adbcc54582cd9eb5f167a2fbaf24d
tswlivia <149760707+tswlivia@users.noreply.github.com>
2023-11-23 19:45:15 +0000

    chang1

--- src/main/java/com/example/devsecops/DevsecopsApplication.java
+++ src/main/java/com/example/devsecops/DevsecopsApplication.java
@@ -2,9 +2,17 @@ package com.example.devsecops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DevsecopsApplication {
+
+	@GetMapping("/") 
+	public String hello() {
+		return "<html><body><h1>Hello DevSecOps</body></h1></html>";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevsecopsApplication.class, args);
